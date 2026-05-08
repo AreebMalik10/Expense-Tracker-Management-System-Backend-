6e22e0c3e5b3f23b7e094d55f723cd01910c5626
Areeb Malik <areeb9273@gmail.com>
2026-05-09 00:57:24 +0500

    updated after pull from main branch

--- src/config/logger.js
+++ src/config/logger.js
@@ -5,7 +5,7 @@ const logger = winston.createLogger({
   level: 'info',
   format: winston.format.simple(),
   transports: [
-    new winston.transports.File({ filename: 'logs/logs.txt', level: 'info' }),
+    new winston.transports.File({ filename: 'logs/app.log', level: 'info' }),
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
   ],
 });
